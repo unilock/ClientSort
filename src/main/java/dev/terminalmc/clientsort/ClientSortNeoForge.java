072175dd68f57999e99542218f83d9e815ad67e5
unilock <unilock@fennet.rentals>
2024-07-09 08:43:34 -0400

    Backport to NeoForge 1.20.4
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/dev/terminalmc/clientsort/ClientSortNeoForge.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 NotRyken
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package dev.terminalmc.clientsort;
+
+import dev.terminalmc.clientsort.screen.ConfigScreenProvider;
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.event.TickEvent;
+
+@Mod(value = ClientSort.MOD_ID)
+@Mod.EventBusSubscriber(modid = ClientSort.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientSortNeoForge {
+    public ClientSortNeoForge() {
+        // Config screen
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, parent) -> ConfigScreenProvider.getConfigScreen(parent)));
+
+        // Main initialization
+        ClientSort.init();
+    }
+
+    // Keybindings
+    @SubscribeEvent
+    static void registerKeyMappingsEvent(RegisterKeyMappingsEvent event) {
+        event.register(ClientSort.SORT_KEY);
+    }
+
+    @Mod.EventBusSubscriber(modid = ClientSort.MOD_ID, value = Dist.CLIENT)
+    static class ClientEventHandler {
+        // Tick events
+        @SubscribeEvent
+        public static void clientTickEvent(TickEvent.ClientTickEvent event) {
+            if (event.phase == TickEvent.Phase.END) {
+                ClientSort.onEndTick(Minecraft.getInstance());
+            }
+        }
+    }
+}
